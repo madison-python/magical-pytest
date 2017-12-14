6242217cd43bc35eb642fab65e47b542d5b5fce1
Daniel Imhoff <dwieeb@gmail.com>
2017-12-13 23:09:10 -0600

    demo 1 result

--- migrating_to_pytest/test_file.py
+++ migrating_to_pytest/test_file.py
@@ -1,14 +1,11 @@
-import unittest
-
-
 def calculate_offset(page, per_page=10):
     return (page - 1) * per_page
 
 
-class TestCalculateOffset(unittest.TestCase):
+class TestCalculateOffset:
 
     def test_first_page(self):
-        self.assertEqual(calculate_offset(1), 0)
+        assert calculate_offset(1) == 0
 
     def test_second_page(self):
-        self.assertEqual(calculate_offset(2), 10)
+        assert calculate_offset(2) == 10
